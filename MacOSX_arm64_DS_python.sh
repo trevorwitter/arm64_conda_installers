120b205aaa36e5f3b59c6ddf2a51b03b0490d92a
trevorwitter <trevorgwitter@gmail.com>
2021-12-16 13:23:57 -0500

    first

---
+++ MacOSX_arm64_DS_python.sh
@@ -0,0 +1,41 @@
+## Native MacOSX arm64 DS environment setup ## 
+
+# Install Mambaforge arm64 specific miniconda distribution
+## Runs native on arm64
+## Note: only packages hosted on conda-forge supported
+cd ~
+
+curl -fsSLo Mambaforge-MacOSX-arm64.sh https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-MacOSX-arm64.sh
+zsh Mambaforge-MacOSX-arm64.sh -b
+conda init zsh
+
+
+# install pytorch (currently only CPU support for M1 chip)
+conda install pytorch torchvision -c pytorch
+
+
+# install sklearn
+brew install openblas
+export OPENBLAS=$(/opt/homebrew/bin/brew --prefix openblas)
+export CFLAGS="-falign-functions=8 ${CFLAGS}"
+# ^ no need to add to .zshrc, just doing this once.
+pip install scikit-learn # ==0.24.1 if you want
+
+
+# install Jupyter and dependencies
+mamba install -c conda-forge jupyterlab
+
+
+# install XGBoost
+conda install cmake llvm-openmp compilers
+pip install xgboost
+
+
+# install Pandas
+conda install pandas
+
+
+# Remove Mambaforge installer
+rm Mambaforge-MacOSX-arm64.sh
+
+
---
+++ README.md
@@ -0,0 +1,21 @@
+# Deep Learning/Machine Learning Environment Installer for M1 (arm64) Mac 
+
+### Run following command from terminal: 
+`curl (this repo)
+zsh MacOSX_arm64_DS_python.sh`
+
+Installs:
+- Mambaforge
+- Jupyter Lab
+- Numpy
+- Pandas
+- skikit-learn
+- XGBoost
+- Pytorch
+
+## Resources
+#### Mambaforge Conda
+[coiled.io blog on mambaforge on M1 Mac](https://coiled.io/blog/apple-arm64-mambaforge/) 
+
+#### Scikit-Learn
+[Stackoverflow post on sklearn for M1 silicon](https://stackoverflow.com/questions/68620927/installing-scipy-and-scikit-learn-on-apple-m1)
